d19355cf6ad0351fa78e42de86176b7f92d1eeac
Egor <example.@icloud.com>
2021-12-20 17:38:29 +0200

    main

---
+++ Homework_8/Homework8.js
@@ -0,0 +1,14 @@
+function upperCase(){
+    let text = 'regexp';
+     text.match(/\w/);
+    // if(){
+        console.log(text);
+        console.log("String's not starts with uppercase character ");
+    //}
+    console.log("String's starts with uppercase character");
+    
+}
+
+upperCase();
+
+
