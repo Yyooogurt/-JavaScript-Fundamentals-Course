57523d77293b5fbf0f24f81b00560442b83c9554
Egor <example.@icloud.com>
2022-01-02 12:35:09 +0200

    main

---
+++ Homework_9/Homework9.js
@@ -0,0 +1,26 @@
+// Task 1
+// let a = $('a[href^="https://"]').attr('target','_blank');
+
+// Task 2
+// $("h2.head").css('background-color', 'green');
+// $("h2.head .inner").css('font-size', '35px');
+
+// Task 3
+// let div1 = $("div:first");
+// let div2 = $("div:last");
+// let h1 = $("h3:first");
+// let h2 = $("h3:last");
+// h2.before(div2);
+// h1.before(div1);
+
+// Task 4
+// let number = 0;
+// let checkboxes = $('input');
+// $("input").on("click", function() {
+//     number += parseInt($('input:checked').val());
+//     if(number == 3){
+//         checkboxes.attr("disabled", true);
+//     }else{
+//         checkboxes.attr("disabled", false);
+//     }
+//   });
